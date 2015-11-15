e4f3b3e09da2e5f9f25e6f3193cda9e53fe4f495
Hendrik Leper <hendrik@HL-MBP.local>
2015-11-15 11:41:44 +0100

    first commit

---
+++ js/map.js
@@ -0,0 +1,56 @@
+jQuery(function($){
+
+	var longitude = 51.050588;
+	var latitude = 3.749223;
+	var canvas = "map";
+
+	
+	function randing_map(canvas, lan, lat){			
+			var myLatlng = new google.maps.LatLng(lan,lat);
+			var myOptions = {
+						zoom: 13,
+						center: myLatlng,
+						mapTypeId: google.maps.MapTypeId.ROADMAP,
+						maxZoom   : 20,
+    					disableDefaultUI: true
+					}			
+			var map = new google.maps.Map( document.getElementById(canvas), myOptions );
+			var marker = new google.maps.Marker({
+			    position : myLatlng,
+			    map      : map,
+			    icon     : "img/point.png"
+			});
+			var styles = [
+			  {
+			    featureType: "all",
+			    stylers: [
+			      { saturation: -80 }
+			    ]
+			  },{
+			    featureType: "road.arterial",
+			    elementType: "geometry",
+			    stylers: [
+			      { hue: "#00ffee" },
+			      { saturation: 50 }
+			    ]
+			  },{
+			    featureType: "poi.business",
+			    elementType: "labels",
+			    stylers: [
+			      { visibility: "off" }
+			    ]
+			  }
+			];	
+			var infowindow = new google.maps.InfoWindow({
+				content:"<div class='map_adresse'><div class='map_address'><span class='address'>Adres : </span>Bouwmeestersstraat 46, 9040 Gent, België </div> <div class='map_tel'><span class='tel'>Een beetje verwarrend : </span>eigenlijk om de hoek op de Klinkkouterstraat</div> <div class='map_tel'><span class='tel'>Route : </span>http://bit.ly/1UKnjt2</div></div>"
+			});	
+			
+			map.setOptions({styles: styles});
+
+			google.maps.event.addListener(marker, 'click', function() {
+			  infowindow.open(map,marker);
+			});
+	}
+	randing_map(canvas, longitude, latitude);
+
+});
